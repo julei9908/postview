03a1bdd5b5be27d0aa2f68579b5ad5ee03492a2a
julei9908 <julei_me@163.com>
2021-06-13 15:43:05 +0800

    更新系统配置

--- src/main/java/org/bluron/postview/PostviewApplication.java
+++ src/main/java/org/bluron/postview/PostviewApplication.java
@@ -1,8 +1,12 @@
 package org.bluron.postview;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan(basePackages={"org.bluron.postview.dao"})
 @SpringBootApplication
 public class PostviewApplication {
 

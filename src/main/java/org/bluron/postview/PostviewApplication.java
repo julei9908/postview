91cb5edb6ac3ef2df2966e929ce456efa1ea17fc
julei9908 <julei_me@163.com>
2021-06-13 14:24:25 +0800

    创建脚手架工程

---
+++ src/main/java/org/bluron/postview/PostviewApplication.java
@@ -0,0 +1,13 @@
+package org.bluron.postview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bluron/postview/PostviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bluron.postview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
